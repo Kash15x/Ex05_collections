6f69979aaff74e780ef930e515106558edb4195a
Ali <Kash@db.com>
2023-07-26 14:24:58 +0100

    update

--- src/com/qa/Main.java
+++ src/com/qa/Main.java
@@ -1,11 +1,8 @@
 package com.qa;
 import com.qa.model.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.TreeMap;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Cat c = new Cat("Whiskers", 10);
@@ -26,65 +23,82 @@ public class Main {
         catList.add(c2);
 
 
-        for (int x = 0; x < animalList.size(); x ++) {
+        for (int x = 0; x < animalList.size(); x++) {
             System.out.println(animalList.get(x));
         }
-        for (var X :animalList){
+        for (var X : animalList) {
             System.out.println(X);
         }
         Iterator<Animal> it = animalList.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             System.out.println(it.next());
         }
 
-         HashMap<String, Animal> animalMap =
-                 new HashMap<String, Animal>();
-         animalMap.put(c.getName(), c);
-         animalMap.put(c2.getName(), c2);
-         animalMap.put(d.getName(), d);
-         animalMap.put(r.getName(), r);
-         animalMap.put(r2.getName(), r2);
+        HashMap<String, Animal> animalMap =
+                new HashMap<String, Animal>();
+        animalMap.put(c.getName(), c);
+        animalMap.put(c2.getName(), c2);
+        animalMap.put(d.getName(), d);
+        animalMap.put(r.getName(), r);
+        animalMap.put(r2.getName(), r2);
 
-          HashMap<Animal, String> animalMap2 = new HashMap<>();
+        HashMap<Animal, String> animalMap2 = new HashMap<>();
 
-          animalMap2.put(c, "Fluffy");
-          animalMap2.put(c2, "Doesn't like being picked up");
-          animalMap2.put(d, "Overly excited about running");
-          animalMap2.put(r, "Snuffles a lot, may have a cold");
-          animalMap2.put(r2, "May actually be evil. Unsure.");
+        animalMap2.put(c, "Fluffy");
+        animalMap2.put(c2, "Doesn't like being picked up");
+        animalMap2.put(d, "Overly excited about running");
+        animalMap2.put(r, "Snuffles a lot, may have a cold");
+        animalMap2.put(r2, "May actually be evil. Unsure.");
 
-          for (String key : animalMap.keySet()) {
-              System.out.println("Key: " + key + " Value: " +
-                      animalMap.get(key));
-          }
-          for(Animal key : animalMap2.keySet()){
-              System.out.println("Key:" + key + "Value:" + animalMap2.get(key
-              ));
-          }
+        for (String key : animalMap.keySet()) {
+            System.out.println("Key: " + key + " Value: " +
+                    animalMap.get(key));
+        }
+        for (Animal key : animalMap2.keySet()) {
+            System.out.println("Key:" + key + "Value:" + animalMap2.get(key
+            ));
+        }
 
-          HashSet<Animal> set = new HashSet<>();
-          //we can just add all the elements of another collection
-          set.addAll(animalList);
+        HashSet<Animal> set = new HashSet<>();
+        //we can just add all the elements of another collection
+        set.addAll(animalList);
 
-           set.add(c); // replaces the previous version of the cat
-            set.add(c2);
-             Iterator<Animal> iter = set.iterator();
-                        while(iter.hasNext()) {
-                            System.out.println(iter.next());
-                        }
+        set.add(c); // replaces the previous version of the cat
+        set.add(c2);
+        Iterator<Animal> iter = set.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
 
-                         System.out.println("\nFinding Spot.");
-                                   for (Animal a : animalList) {
-                                       if (a.getName().equals("Spot")) {
-                                           System.out.println(
-                                                   "Found Spot in the ArrayList: " + a);
-                                       }
-                                   }
-                                   System.out.println("Found Spot in the HashMap: " +
-                                                                         animalMap.get("Spot"));
+        System.out.println("\nFinding Spot.");
+        for (Animal a : animalList) {
+            if (a.getName().equals("Spot")) {
+                System.out.println(
+                        "Found Spot in the ArrayList: " + a);
+            }
+        }
+        System.out.println("Found Spot in the HashMap: " +
+                animalMap.get("Spot"));
 
-                                                      }
-                                   }
+        Collections.sort(animalList);
+        System.out.println("Sorted animal list");
+        for (int x = 0; x < animalList.size(); x++) {
+
+
+            System.out.println(animalList.get(x));
+
+        }
+
+        TreeMap<String, Animal> tree = new TreeMap<>();
+                   tree.putAll(animalMap);
+
+                   System.out.println("TreeMap");
+                              for (String key : tree.keySet()) {
+                                  System.out.println("Key: " + key + " Value: " +
+                                          tree.get(key));
+                              }
+                              }
+    }
 
 
 
